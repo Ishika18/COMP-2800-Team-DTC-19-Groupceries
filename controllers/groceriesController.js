d60aed535413bd278ba7865c88017f0d899f9240
Ishika18 <shagunphw@gmail.com>
2020-04-26 14:17:23 -0700

    Shagun - got the app navbar working

--- controllers/groceriesController.js
+++ controllers/groceriesController.js
@@ -1,4 +1,25 @@
 const firebase = require("firebase/app");
+require('firebase/firestore');
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC59-CinZU4yGdTnIcQPXEaIOC5R7cGfLA",
+    authDomain: "groupceries-f6189.firebaseapp.com",
+    databaseURL: "https://groupceries-f6189.firebaseio.com",
+    projectId: "groupceries-f6189",
+    storageBucket: "groupceries-f6189.appspot.com",
+    messagingSenderId: "688161851064",
+    appId: "1:688161851064:web:23a0bc2b403535a57f964d"
+};
+
+// Initialize Firebase
+// make sure it is not intialised before.
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+let database = firebase.firestore();
 
 let groceriesController = {
 
@@ -8,12 +29,12 @@ let groceriesController = {
 
     createListPage: (req, res) => {
         // instead of grocery list user email could be passed because firestore data has to be edited.
-        res.render('groceries/createListPage', {groceryList: database.randomUserId.groceries})
+        res.render('groceries/createListPage', database);
     },
 
     editListPage: (req, res) => {
         // instead of grocery list user email could be passed because firestore data has to be edited.
-        res.render('groceries/editListPage', {groceryList: database.randomUserId.groceries})
+        res.render('groceries/editListPage', database);
     }
 };
 
