1c4527ecc3612e083b573f3a5aeeeef3c0d77a7d
Ishika18 <shagunphw@gmail.com>
2020-04-26 14:51:56 -0700

    Shagun -  firebase intialised.

--- app.js
+++ app.js
@@ -3,39 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const ejsLayouts = require('express-ejs-layouts');
 const groceriesController = require('./controllers/groceriesController');
+const authController = require('./controllers/authController');
 
-const firebase = require('firebase/app');
-require('firebase/auth');
-
-const firebaseConfig = {
-    apiKey: "AIzaSyC59-CinZU4yGdTnIcQPXEaIOC5R7cGfLA",
-    authDomain: "groupceries-f6189.firebaseapp.com",
-    databaseURL: "https://groupceries-f6189.firebaseio.com",
-    projectId: "groupceries-f6189",
-    storageBucket: "groupceries-f6189.appspot.com",
-    messagingSenderId: "688161851064",
-    appId: "1:688161851064:web:23a0bc2b403535a57f964d"
-};
-
-// Initialize Firebase
-// make sure it is not initialized before
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
-const loggerMiddleware = (req, res, next) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            // user is signed in 
-            console.log(user);
-            // do the rendering or whatever
-            next();
-        } else {
-            // redirect to the home page.
-            res.render("home.ejs");
-        }
-    });
-};
+const loggerMiddleware = authController.loggerMiddleware;
+const user = authController.user;
 
 const PORT = process.env.PORT || 3000;
 
