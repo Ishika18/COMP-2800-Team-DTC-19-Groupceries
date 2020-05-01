5a0ceec1d98a9519c275c7f0b2731ef656113bed
Silvana <skalliny@my.bcit.ca>
2020-04-30 21:34:37 -0400

    fixed bug in add item function

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,5 +1,6 @@
 function newItemField() {
     let item = document.createElement("div")
+    item.className = "listItems"
     let list = document.getElementById("groceryList")
     list.appendChild(item)
     var fields = ["Name", "Quantity", "Units", "Notes(Optional)"]
@@ -9,13 +10,39 @@ function newItemField() {
         input = document.createElement("input")
         input.setAttribute("type", "text")
         input.id = fields[i]
+        input.classList = "textInput"
         label = document.createElement("label")
         label.innerHTML = fields[i]
         item.appendChild(label)
         item.appendChild(input)
     }
+        addButton = document.createElement("button")
+        addButton.innerHTML = "Add"
+        addButton.id = "addButton"
+        item.appendChild(addButton)
+        addButton.onclick = addItemDetails(item)
+    }
 
-document.getElementById("newItem").onclick = newItemField()
+document.getElementById("newItem").onclick = newItemField
+
+       function addItemDetails(item) {
+        return function() {
+            var fields = item.getElementsByClassName("textInput")
+            var fieldsCopy = []
+            for (field in fields) {
+                fieldsCopy[field] = fields[field] //create shallow copy to prevent errors related to list length
+            }
+            var i
+
+            for (i=0; i< fieldsCopy.length; i++) {
+                input = fieldsCopy[i]
+                inputData = input.value
+                inputText = document.createElement("p")
+                inputText.innerHTML = inputData
+                item.replaceChild(inputText, input)
+            }
+        }
+    }
 
     // let item = document.createElement("div")
     // let list = document.getElementById("groceryList")
@@ -54,4 +81,4 @@ document.getElementById("newItem").onclick = newItemField()
     // editButton.innerHTML = "Edit"
     // item.appendChild(editButton)
     // editButton.id = "edit"
- }
+ 
