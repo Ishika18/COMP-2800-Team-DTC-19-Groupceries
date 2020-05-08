d5e17706497c6474545c1ed525ec633b3f1840f2
Shagun <shagunphw@gmail.com>
2020-05-07 20:55:07 -0700

    Shagun - accept and decline friend request feature.

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -54,6 +54,33 @@ let friendController = {
         currentUserDocs.update({ sent: firebase.firestore.FieldValue.arrayRemove(friendID) }).catch((error) => { console.log(error) });
     },
 
+    acceptFriendRequest: (currentUser, friendID) => {
+        // document friends of current user
+        let currentUserDocs = db.collection(currentUser).doc("Friends");
+
+        // document friends of friend
+        let friendDocs = db.collection(friendID).doc("Friends");
+
+        // add the current uid to the friends' accepted
+        friendDocs.set({ accepted: firebase.firestore.FieldValue.arrayUnion(currentUser) }, { merge: true }).catch((error) => { console.log(error) });
+
+        // send the friend a notification
+
+        // add the friend uid to the cuurent users' accepted
+        currentUserDocs.set({ accepted: firebase.firestore.FieldValue.arrayUnion(friendID) }, { merge: true }).catch((error) => { console.log(error) });
+    },
+
+    declineFriendRequest: (currentUser, friendID) => {
+        /* If the user declines, should we inform the user who sent the friend request ?*/
+        // delete the current email or uid to the friends' sent
+        friendDocs.update({ sent: firebase.firestore.FieldValue.arrayRemove(currentUser) }).catch((error) => { console.log(error) });
+
+        // remove the notification (maybe ?)
+
+        // delete the friend email or uid to the cuurent users' recieved
+        currentUserDocs.update({ recieved: firebase.firestore.FieldValue.arrayRemove(friendID) }).catch((error) => { console.log(error) });
+    },
+
     
 }
 
