0ea51e9968107d858ef5f2e835682a4ba58d2672
Shagun <shagunphw@gmail.com>
2020-05-13 15:06:23 -0700

    Shagun - undo sent friend request

--- public/js/sentFriends.js
+++ public/js/sentFriends.js
@@ -1,3 +1,20 @@
+function undoSentRequest(friendID) {
+    let currentUser = window.localStorage.getItem('uid');
+    // document friends of current user
+    let currentUserDocs = db.collection(window.localStorage.getItem('uid')).doc("Friends");
+
+    // document friends of friend
+    let friendDocs = db.collection(friendID).doc("Friends");
+
+    // delete the current uid to the friends'recieved
+    friendDocs.update({ received: firebase.firestore.FieldValue.arrayRemove(currentUser) }).catch((error) => { console.log(error) });
+
+    // remove the notification (maybe ?)
+
+    // delete the friend email or uid to the cuurent users' sent
+    currentUserDocs.update({ sent: firebase.firestore.FieldValue.arrayRemove(friendID) }).catch((error) => { console.log(error) });
+}
+
 function addHTML(uid) {
     db.collection(uid).doc('userInfo').get().then((doc) => {
         if (doc.exists) {
@@ -18,7 +35,7 @@ function generateHTML(name, email, uid) {
       <div class="col-sm">
           <!-- The id for buttons are very important -->
           <!-- Please don't change the id -->
-          <button id="${uid}_S" onclick="undoSentRequest()">Undo Request</button>
+          <button id="${uid}_S" onclick="undoSentRequest('${uid}')">Undo Request</button>
       </div>
     </div>
   </div>
